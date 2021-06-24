44693ba8da3e7d7dc9e4d96d9441cf149cb3bca5
Kamil Sularz <kamilsularz5@gmail.com>
2021-06-24 15:53:00 +0200

    Initial Commit

---
+++ app/src/main/java/pl/wroc/edu/a54321/projectfin/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.wroc.edu.a54321.projectfin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
